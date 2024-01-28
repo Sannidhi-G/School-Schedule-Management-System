4b219f15a6b05c338da78fe26ba2a65953047131
Sannidhi-G <sannidhi2001smg@gmail.com>
2024-01-28 23:41:48 +0530

    initial commit

---
+++ src/main/java/com/jsp/school/SchoolBoardAPI/SchoolBoardApi1Application.java
@@ -0,0 +1,13 @@
+package com.jsp.school.SchoolBoardAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBoardApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolBoardApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/school/SchoolBoardAPI/SchoolBoardApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.school.SchoolBoardAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBoardApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
